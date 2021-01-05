1af2d36dce7c28966e4b9fd337af97a9d229256a
info-sapphire <info.sapphire@yandex.ru>
2021-01-05 22:16:26 +0300

    feat: add action create post

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,62 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TextInput,
+  Image,
+  Button,
+  ScrollView,
+  Keyboard,
+  View
+} from 'react-native'
+import { useDispatch } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import { AppHeaderIcon } from '../components/AppHeaderIcon'
+import { THEME } from '../theme'
+import { addPost } from '../store/actions/post'
 
-export const CreateScreen = ({}) => {
+export const CreateScreen = ({ navigation }) => {
+  const [text, setText] = useState('')
+  const dispatch = useDispatch()
+
+  const img =
+    'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg'
+
+  const saveHandler = () => {
+    dispatch(
+      addPost({
+        text,
+        img,
+        date: new Date().toJSON()
+      })
+    )
+    setText('')
+    navigation.navigate('Main')
+  }
+
   return (
-    <View style={styles.wrapper}>
-      <Text>CreateScreen</Text>
-    </View>
+    <ScrollView>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>Создать новый пост</Text>
+          <TextInput
+            style={styles.textarea}
+            placeholder="Введите текст заметки"
+            value={text}
+            onChangeText={setText}
+            multiline
+          />
+          <Image style={styles.image} source={{ uri: img }} />
+          <Button
+            title="Создать заметку"
+            color={THEME.MAIN_COLOR}
+            onPress={saveHandler}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   )
 }
 
@@ -27,8 +75,24 @@ CreateScreen.navigationOptions = ({ navigation }) => ({
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    padding: 10
+  },
+
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: 'open-regular',
+    marginVertical: 10
+  },
+
+  textarea: {
+    padding: 10,
+    marginBottom: 10
+  },
+
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10
   }
 })

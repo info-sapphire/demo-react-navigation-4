4aabdd04d318f81966d3b278b333bb0657e4fc13
info-sapphire <info.sapphire@yandex.ru>
2021-01-08 00:12:43 +0300

    feat: add loading state

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
+import { View, StyleSheet, ActivityIndicator } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { AppHeaderIcon } from '../components/AppHeaderIcon'
 import { PostList } from '../components/PostList'
 import { loadPosts } from '../store/actions/post'
+import { THEME } from '../theme'
 
 export const MainScreen = ({ navigation }) => {
   const openPostHandler = post => {
@@ -16,11 +18,20 @@ export const MainScreen = ({ navigation }) => {
   }
 
   const allPosts = useSelector(state => state.post.allPosts)
+  const loading = useSelector(state => state.post.loading)
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(loadPosts())
   }, [dispatch])
+
+  if (loading) {
+    return (
+      <View style={styles.wrapper}>
+        <ActivityIndicator color={THEME.MAIN_COLOR} />
+      </View>
+    )
+  }
 
   return <PostList data={allPosts} onOpen={openPostHandler} />
 }
@@ -45,4 +56,12 @@ MainScreen.navigationOptions = ({ navigation }) => ({
       />
     </HeaderButtons>
   )
+})
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 })
